83c087de9e0e6089b032c22cd8f56f76b9fc1f1c
John Barboza <jbarboza@ca.ibm.com>
2016-08-31 19:00:13 -0400

    zos: read free/total memory from mvs data areas

--- src/unix/os390.c
+++ src/unix/os390.c
@@ -22,6 +22,33 @@
 #include "internal.h"
 #include <unistd.h>
 
+#define CVT_PTR           0x10
+#define CSD_OFFSET        0x294
+
+/* "V(IARMRRCE)" - ADDRESS OF THE RSM CONTROL AND ENUMERATION AREA. */
+#define CVTRCEP_OFFSET    0x490
+
+/* 
+    NUMBER OF FRAMES CURRENTLY AVAILABLE TO SYSTEM. 
+    EXCLUDED ARE FRAMES BACKING PERM STORAGE, FRAMES OFFLINE, AND BAD FRAMES
+*/
+#define RCEPOOL_OFFSET    0x004
+
+/* TOTAL NUMBER OF FRAMES CURRENTLY ON ALL AVAILABLE FRAME QUEUES. */
+#define RCEAFC_OFFSET     0x088
+
+typedef unsigned data_area_ptr_assign_type;
+
+typedef union {
+  struct {
+#if defined(_LP64)
+    data_area_ptr_assign_type lower;
+#endif
+    data_area_ptr_assign_type assign;
+  };
+  char* deref;
+} data_area_ptr; 
+
 void uv_loadavg(double avg[3]) {
 
   //TODO: implement the following
@@ -116,6 +143,24 @@ int uv_exepath(char* buffer, size_t* size) {
     /* Out of tokens (path entries), and no match found */
     return -EINVAL;
   }
+}
+
+uint64_t uv_get_free_memory(void) {
+  data_area_ptr cvt = {0};
+  data_area_ptr rcep = {0};
+  cvt.assign = *(data_area_ptr_assign_type*)(CVT_PTR);
+  rcep.assign = *(data_area_ptr_assign_type*)(cvt.deref + CVTRCEP_OFFSET);
+  uint64_t freeram = *((uint64_t*)(rcep.deref + RCEAFC_OFFSET)) * 4;
+  return freeram;
+}
+
+uint64_t uv_get_total_memory(void) {
+  data_area_ptr cvt = {0};
+  data_area_ptr rcep = {0};
+  cvt.assign = *(data_area_ptr_assign_type*)(CVT_PTR);
+  rcep.assign = *(data_area_ptr_assign_type*)(cvt.deref + CVTRCEP_OFFSET);
+  uint64_t totalram = *((uint64_t*)(rcep.deref + RCEPOOL_OFFSET)) * 4;
+  return totalram;
 }
 
 
